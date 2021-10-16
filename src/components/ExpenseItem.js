7ef4b7892cf8d6306585a2521b6b3765114592c2
Sejmou <samo.kolter@gmail.com>
2021-10-16 14:42:17 +0200

    first component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {// React components get all input props via props object
+
+    return (
+        <div className="expense-item">
+            <div>{props.date.toISOString()}</div>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">{props.amount}</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
