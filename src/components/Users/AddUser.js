3659e60ae0fe70356a98e1af5221bbe134435305
Sejmou <samo.kolter@gmail.com>
2021-12-23 17:02:31 +0100

    Wrapper helper component for rendering adjacent elements without wrapping div

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,14 +3,16 @@ import React, { useState } from 'react';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import ErrorModal from '../UI/ErrorModal';
+import Wrapper from '../Helpers/Wrapper';
+
 import classes from './AddUser.module.css';
 
-const AddUser = (props) => {
+const AddUser = props => {
   const [enteredUsername, setEnteredUsername] = useState('');
   const [enteredAge, setEnteredAge] = useState('');
   const [error, setError] = useState();
 
-  const addUserHandler = (event) => {
+  const addUserHandler = event => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
       setError({
@@ -31,11 +33,11 @@ const AddUser = (props) => {
     setEnteredAge('');
   };
 
-  const usernameChangeHandler = (event) => {
+  const usernameChangeHandler = event => {
     setEnteredUsername(event.target.value);
   };
 
-  const ageChangeHandler = (event) => {
+  const ageChangeHandler = event => {
     setEnteredAge(event.target.value);
   };
 
@@ -44,7 +46,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <div>
+    <Wrapper>
       {error && (
         <ErrorModal
           title={error.title}
@@ -71,7 +73,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
