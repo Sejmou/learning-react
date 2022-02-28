fb6d911d9683eb043c991d2388052dc8b695cf81
Sejmou <samo.kolter@gmail.com>
2022-02-28 20:52:27 +0100

    useCallback as solution for issues with callbacks as props + React.memo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import Button from './components/UI/Button/Button';
 import DemoOutput from './components/Demo/DemoOutput';
 
@@ -8,9 +8,14 @@ function App() {
   const [showParagraph, setShowParagraph] = useState(false);
   console.log('App reevaluated!');
 
-  const toggleParagraphHandler = () => {
+  // useCallback solves our issue with "wrong prop change detection" for functions passed as props
+  // the reference to the function object passed to the Button will stay the same
+  // no structuraly identical function object (but with different reference!) will be recreated on component reevaluation!
+  // React will store the function internally and always pass the same callback on as prop
+  // The only exception would be if any dependency in the array we pass as second argument changed (empty in our case -> no deps!)
+  const toggleParagraphHandler = useCallback(() => {
     setShowParagraph(prevShowParagraph => !prevShowParagraph);
-  };
+  }, []);
 
   return (
     <div className="app">
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -21,4 +21,5 @@ const Button = props => {
 // Notice how, contrary to DemoOutput, this component function gets reevaluated even though seemingly nothing changed in the props!?
 // This has to do with the fact that "By default it will only shallowly compare complex objects in the props object" (https://reactjs.org/docs/react-api.html#reactmemo)
 // So, per default memo works in the way one would intuitively expect only with primitive types but not with objects (and therefore also not with functions or arrays!)
+// We get around this issue by using the useCallback hook (see App component comment for details)
 export default React.memo(Button);
