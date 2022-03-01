691b967d4954ff40d233bb647b62ab9c2a8c9402
Sejmou <samo.kolter@gmail.com>
2022-03-01 13:58:32 +0100

    add some comments about useState, fix error using useCallback for allowTogleHandler

--- src/App.js
+++ src/App.js
@@ -5,21 +5,16 @@ import DemoOutput from './components/Demo/DemoOutput';
 import './App.css';
 
 function App() {
+  // Q: why is state not "recreated" on every call to useState()?
+  // A: Upon first execution of component function, React stores some kind of mapping of component instance -> state
+  // Therefore, the default value of useState is only really taken into account when the component function is executed the first time
+  // Basically, the default value assignment is then always "skipped", except if a component was completely removed from the DOM in the meantime?
   const [allowToggle, setAllowToggle] = useState(false);
   const [showParagraph, setShowParagraph] = useState(false);
   console.log('App reevaluated!');
 
-  const allowToggleHandler = useCallback(() => setAllowToggle(true));
+  const allowToggleHandler = useCallback(() => setAllowToggle(true), []);
 
-  // this demonstrates why the dependencies array in useCallback is actually useful/needed
-  // this has to do with JavaScript closures
-  // every function in JS is able to access vars from the scope it was defined in (+ "parent" scopes) as well as global variables
-  // this means it kinda "caches" variable values
-  // if we would leave the allowToggle variable out of the dependencies array, the toggleParagraphHandler callback would be stored in the first run of the component
-  // This created callback function would then store the initial value for allowToggle!
-  // It would not notice changes to the allowToggle variable as those would actually be different references to newly created allowToggle variables of each run?
-  // Instead, it would use the "cached" allowToggle variable which was false and would stay false forever!
-  // adding the allowToggle dep would recreate the callback with the most recent allowToggle value every time allowToggle changes
   const toggleParagraphHandler = useCallback(() => {
     if (allowToggle) setShowParagraph(prevShowParagraph => !prevShowParagraph);
   }, [allowToggle]);
