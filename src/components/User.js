c156ba5954f19dfdb77a706b99be2ba4e1a2e1b7
Sejmou <samo.kolter@gmail.com>
2022-07-17 15:14:25 +0200

    componentWillUnmount() lifecycle hook

--- src/components/User.js
+++ src/components/User.js
@@ -6,6 +6,12 @@ class User extends Component {
   render() {
     return <li className={classes.user}>{this.props.name}</li>;
   }
+
+  componentWillUnmount() {
+    // called before component unmounts
+    // rouhgly equivalent to cleanup function in useEffect with empty deps array
+    console.log(`User ${this.props.name} will unmount`);
+  }
 }
 
 // const User = (props) => {
