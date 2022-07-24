49e7fc8d02275fa2c4952fe37db25894d1201e60
Sejmou <samo.kolter@gmail.com>
2022-07-24 17:23:38 +0200

    very basic custom hook (without config params - useCounter)

--- src/hooks/use-counter.js
+++ src/hooks/use-counter.js
@@ -1,1 +1,21 @@
-// TODO
+import { useState, useEffect } from 'react';
+
+// Note: you HAVE to use the "use" prefix to tell React that this function is a custom hook
+// using this name also implies several hook-specific restrictions
+const useCounter = () => {
+  // state defined by a custom hook will be created/managed separately for each component that uses it
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter(prevCounter => prevCounter + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  // custom hooks may return whatever we want (primitive value, object, array, ...)
+  return counter;
+};
+
+export default useCounter;
