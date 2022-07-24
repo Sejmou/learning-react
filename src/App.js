6a2605172b8e9c08c25d97dc245f92d3693d1de7
Sejmou <samo.kolter@gmail.com>
2022-07-24 17:00:55 +0200

    simple POST excample (using Firebase for storing movies)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
@@ -13,19 +14,29 @@ function App() {
       setIsLoading(true);
       setError(null);
       try {
-        const response = await fetch('https://swapi.dev/api/films/');
+        const response = await fetch(
+          'https://react-course-schwarzmueller-default-rtdb.europe-west1.firebasedatabase.app/movies.json'
+        );
         if (!response.ok) {
           throw new Error('Something went wrong!');
         }
 
         const data = await response.json();
-        const transformedMovies = data.results.map(movieData => ({
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        }));
-        setMovies(transformedMovies);
+
+        console.log(data);
+
+        const loadedMovies = Object.entries(data).map(([id, value]) => {
+          const { title, openingText, releaseDate } = value;
+          return {
+            id,
+            title,
+            openingText,
+            releaseDate,
+          };
+        });
+        console.log(loadedMovies);
+
+        setMovies(loadedMovies);
       } catch (error) {
         setError(error.message);
       }
@@ -34,6 +45,20 @@ function App() {
     }
     fetchMovies();
   }, []);
+
+  const addMovieHandler = async movie => {
+    console.log(movie);
+    const res = await fetch(
+      'https://react-course-schwarzmueller-default-rtdb.europe-west1.firebasedatabase.app/movies.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(movie),
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+    const data = await res.json();
+    console.log(data);
+  };
 
   useEffect(() => fetchMoviesHandler(), [fetchMoviesHandler]); // listing fetchMoviesHandler as dep. is not really necessary here, but useful if the handler relies on external state
 
@@ -53,6 +78,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
