bc3ed7629aac1e4a5dafa2e9154a650d713b8627
Sejmou <samo.kolter@gmail.com>
2022-07-25 13:08:30 +0200

    simple example for custom hook with parameters

--- src/hooks/use-counter.js
+++ src/hooks/use-counter.js
@@ -2,17 +2,18 @@ import { useState, useEffect } from 'react';
 
 // Note: you HAVE to use the "use" prefix to tell React that this function is a custom hook
 // using this name also implies several hook-specific restrictions
-const useCounter = () => {
+// hooks may accept parameters like any other regular function
+const useCounter = (forward = true) => {
   // state defined by a custom hook will be created/managed separately for each component that uses it
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter(prevCounter => prevCounter + 1);
+      setCounter(prevCounter => (forward ? prevCounter + 1 : prevCounter - 1));
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [forward]); // "hook parameters" used here must also be stated as dependency!
 
   // custom hooks may return whatever we want (primitive value, object, array, ...)
   return counter;
