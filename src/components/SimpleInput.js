7544a51d77da66f848bdfbd6680d3e028bfc63d8
Sejmou <samo.kolter@gmail.com>
2022-07-27 12:03:36 +0200

    basic form input validation and styling

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -8,6 +8,7 @@ const SimpleInput = props => {
   // reasoning behind that: "If you're manipulating the DOM directly, why are you even using React?"
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState('');
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
 
   const nameInputChangeHandler = event => {
     setEnteredName(event.target.value);
@@ -15,14 +16,25 @@ const SimpleInput = props => {
 
   const formSubmissionHandler = event => {
     event.preventDefault(); // prevent default behavior of submitting to server serving the HTML -> would result in page reload and losing state
+
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false);
+      return;
+    }
+    setEnteredNameIsValid(true);
+
     console.log('current value of state variable for name:', enteredName);
     const inputRefVal = nameInputRef.current.value; // current stores reference to input element -> access its value
     console.log('current value of input ref:', inputRefVal);
   };
 
+  const nameInputClasses = enteredNameIsValid
+    ? 'form-control'
+    : 'form-control invalid';
+
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
           type="text"
@@ -30,6 +42,9 @@ const SimpleInput = props => {
           onChange={nameInputChangeHandler}
           ref={nameInputRef} // in practice wwe would not use both change handler and ref!
         />
+        {!enteredNameIsValid && (
+          <p className="error-text">Name must not be empty!</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
