1cf7225d630d7d20455929c63d44519245942699
Sejmou <samo.kolter@gmail.com>
2022-07-27 15:03:41 +0200

    fetching meals from Firebase w/ useBackend hook (my solution)

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,76 @@
+import { useEffect, useState } from 'react';
+
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
+import useBackend from '../../hooks/use-backend';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
+// const DUMMY_MEALS = [
+//   {
+//     id: 'm1',
+//     name: 'Sushi',
+//     description: 'Finest fish and veggies',
+//     price: 22.99,
+//   },
+//   {
+//     id: 'm2',
+//     name: 'Schnitzel',
+//     description: 'A german specialty!',
+//     price: 16.5,
+//   },
+//   {
+//     id: 'm3',
+//     name: 'Barbecue Burger',
+//     description: 'American, raw, meaty',
+//     price: 12.99,
+//   },
+//   {
+//     id: 'm4',
+//     name: 'Green Bowl',
+//     description: 'Healthy...and green...',
+//     price: 18.99,
+//   },
+// ];
+
+// Those meals were stored on Firebase as JSON object under the meals key, the object looks like this:
+// {
+//   "m1": {
+//     "name": "Sushi",
+//     "description": "Finest fish and veggies",
+//     "price": 22.99
+//   },
+//   "m2":{
+//     "name": "Schnitzel",
+//     "description": "A german specialty!",
+//     "price": 16.5
+//   },
+//   "m3":{
+//     "name": "Barbecue Burger",
+//     "description": "American, raw, meaty",
+//     "price": 12.99
+//   },
+//   "m4":{
+//     "name": "Green Bowl",
+//     "description": "Healthy...and green...",
+//     "price": 18.99
+//   }
+// }
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  const { sendRequest, isLoading, error } = useBackend();
+
+  useEffect(() => {
+    sendRequest({ endpoint: 'meals' }, data => {
+      const extractedMeals = Object.entries(data).map(([id, value]) => ({
+        id,
+        ...value,
+      }));
+      setMeals(extractedMeals);
+    });
+  }, [sendRequest]);
+
+  const mealsList = meals.map(meal => (
     <MealItem
       key={meal.id}
       id={meal.id}
@@ -43,7 +83,7 @@ const AvailableMeals = () => {
   return (
     <section className={classes.meals}>
       <Card>
-        <ul>{mealsList}</ul>
+        {meals.length > 0 ? <ul>{mealsList}</ul> : 'Could not fetch meals :/'}
       </Card>
     </section>
   );
---
+++ src/hooks/use-backend.js
@@ -0,0 +1,46 @@
+import { useState, useCallback } from 'react';
+
+const useBackend = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  // logic is simple: if body is provided in requestConfig, a POST request is sent to the Firebase backend at the provided endpoint
+  // else, we do a GET request on the provided endpoint and return the data as a JS object
+  // optional TODO: proper type checks etc. - won't do for this project
+  const sendRequest = useCallback(async (requestConfig, handleData) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      console.log('making request to Firebase with config', requestConfig);
+      console.log('handleData', handleData);
+      const response = await fetch(
+        `https://react-course-schwarzmueller-default-rtdb.europe-west1.firebasedatabase.app/${requestConfig.endpoint}.json`,
+        {
+          method: requestConfig.body ? 'POST' : 'GET',
+          headers: { 'Content-Type': 'application/json' },
+          body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Request failed!');
+      }
+
+      const data = await response.json();
+
+      handleData(data);
+    } catch (err) {
+      console.error('Error occured while sending request', err);
+      setError(err.message || 'Something went wrong!');
+    }
+    setIsLoading(false);
+  }, []);
+
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
+};
+
+export default useBackend;
