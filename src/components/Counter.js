925b74fd9c14919f31c499a29a2237ee1a5b2c57
Sejmou <samo.kolter@gmail.com>
2022-07-29 11:26:18 +0200

    simple action payload example (additional props besides type)

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,48 +1,36 @@
-import { Component } from 'react';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import classes from './Counter.module.css';
 
-class Counter extends Component {
-  incrementHandler() {
-    this.props.increment();
-  }
-  decrementHandler() {
-    this.props.decrement();
-  }
+const Counter = () => {
+  const counter = useSelector(state => state.counter);
 
-  toggleCounterHandler() {}
+  const dispatch = useDispatch();
 
-  render() {
-    return (
-      <main className={classes.counter}>
-        <h1>Redux Counter</h1>
-        <div className={classes.value}>{this.props.counter}</div>
-        <div>
-          <button onClick={this.incrementHandler.bind(this)}>Increment</button>
-          <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
-        </div>
-        <button onClick={this.toggleCounterHandler.bind(this)}>
-          Toggle Counter
-        </button>
-      </main>
-    );
-  }
-}
-
-// the effect of this is basically the same as the useSelector() call when using hooks in functional components?
-const mapStateToProps = state => {
-  return { counter: state.counter };
-};
-
-// ... and this is basically equivalent to useDispatch
-const mapDispatchToProps = dispatch => {
-  return {
-    increment: () => dispatch({ type: 'increment' }),
-    decrement: () => dispatch({ type: 'decrement' }),
+  const incrementHandler = () => {
+    dispatch({ type: 'increment' });
   };
+  const increaseHandler = () => {
+    dispatch({ type: 'increase', amount: 5 });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: 'decrement' });
+  };
+
+  const toggleCounterHandler = () => {};
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
 };
 
-// connect() is a function that returns a new, enhanced component to which we should provide our component as argument
-// as a result of this, both the redux state and actions we provided in the mapping functions will be available as props in the component
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+export default Counter;
