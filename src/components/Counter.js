1799d419cd85c6c98f5e599ecbc0fed9df31b3e8
Sejmou <samo.kolter@gmail.com>
2022-07-29 11:08:38 +0200

    dispatching actions from components w/ useDispatch

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,4 +1,4 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import classes from './Counter.module.css';
 
@@ -9,12 +9,25 @@ const Counter = () => {
   // the subscription is also automatically cleared if the component unmounts
   const counter = useSelector(state => state.counter);
 
+  const dispatch = useDispatch(); // the function returned by this hook is used for dispatching actions to the store
+
+  const incrementHandler = () => {
+    dispatch({ type: 'increment' });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: 'decrement' });
+  };
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
