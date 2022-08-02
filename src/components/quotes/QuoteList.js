65cb5cd4130bdc98f602011b4f88939089c74636
Sejmou <samo.kolter@gmail.com>
2022-08-02 12:02:26 +0200

    sorting controlled by query params

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,13 +1,31 @@
 import { Fragment } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
-const QuoteList = (props) => {
+const QuoteList = props => {
+  const history = useHistory();
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search); // makes queryParams accesible via get()
+
+  const isSortingAscending = queryParams.get('sort') === 'asc';
+
+  const changeSortingHandler = () => {
+    history.push(`/quotes?sort=${isSortingAscending ? 'desc' : 'asc'}`);
+  };
+
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>
+          Sort {isSortingAscending ? 'Descending' : 'Ascending'}
+        </button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map(quote => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
@@ -19,5 +37,12 @@ const QuoteList = (props) => {
     </Fragment>
   );
 };
+
+function sortQuotes(quotes, ascending) {
+  // using spread syntax to make sure copy of original array is sorted (not essential here)
+  return [...quotes].sort((a, b) =>
+    ascending ? (a.id > b.id ? 1 : -1) : a.id < b.id ? 1 : -1
+  );
+}
 
 export default QuoteList;
