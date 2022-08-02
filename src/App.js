86641e76bdb31e1044af711750ccc84881047b5c
Sejmou <samo.kolter@gmail.com>
2022-08-02 20:45:26 +0200

    example #2: alternative syntax for nested routes

--- src/App.js
+++ src/App.js
@@ -16,7 +16,14 @@ function App() {
                 i.e. the intermediate route (/ in this case) would remain if the user were to click the back button in the browser */}
             <Route path="/" element={<Navigate replace to="/welcome" />} />
             {/* Change #8: For nested routes to work it is necessary to use the wildcard syntax with "*" */}
-            <Route path="/welcome/*" element={<Welcome />} />
+            {/* Change #10: Nested routes can also be defined directly in the parent component (e.g. App.js)
+                Simply use nested routes as children of Route!
+                To define where in component nested routes should be output, use Outlet in it */}
+            <Route path="/welcome/*" element={<Welcome />}>
+              {/* Change #9: paths in nested routes AND links are now relative to parent!
+                  i.e. instead of /welcome/new-user we write new-user */}
+              <Route path="new-user" element={<p>Welcome, new user!</p>} />
+            </Route>
             <Route path="/products" element={<Products />} />
             <Route path="/products/:productId" element={<ProductDetail />} />
           </Routes>
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,15 +1,12 @@
-import { Link, Route, Routes } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 const Welcome = () => {
   return (
     <>
       <h1>Welcome!</h1>
       {/* Change #7: Nested routes now have to be wrapped with Routes, even when it's just a single one */}
-      <Routes>
-        {/* Change #9: paths in nested routes AND links are now relative to parent!
-            i.e. instead of /welcome/new-user we write new-user */}
-        <Route path="new-user" element={<p>Welcome, new user!</p>} />
-      </Routes>
+      {/* Related to Change #10 (see App.js): We need an Outlet to specify where components of nested routes should be rendered */}
+      <Outlet />
       {/* Example for relative link */}
       <Link to="new-user">New User? Click here</Link>
       {/* Of course, we can also specify absolute links */}
