e42d8c1e0dd18b39b0c09ab33e9b208a8bb83258
Sejmou <samo.kolter@gmail.com>
2022-08-02 18:37:12 +0200

    fetching quotes on AllQuotes and QuoteDetail pages

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,30 +1,51 @@
+import { useEffect } from 'react';
 import { Route, Link, useRouteMatch } from 'react-router-dom';
 
 import Comments from '../components/comments/Comments';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
-
-// will be removed later once we use a backend to fetch actual data
-const DUMMY_QUOTES = [
-  { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
-  { id: 'q2', author: 'Maximilian', text: 'Learning React is great!' },
-];
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+import useHttp from '../hooks/use-http';
+import { getSingleQuote } from '../lib/api';
 
 const QuoteDetail = () => {
   const match = useRouteMatch();
   const quoteId = match.params.quoteId;
 
-  const quote = DUMMY_QUOTES.find(quote => quote.id === quoteId);
+  const {
+    status,
+    sendRequest,
+    error,
+    data: loadedQuote,
+  } = useHttp(getSingleQuote, true);
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
   const quotePath = match.path;
   const commentsPath = `${quotePath}/comments`;
   const commentsURL = `${match.url}/comments`;
 
-  if (!quote) {
-    return <p>Quote not found :/</p>;
+  if (status === 'pending') {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
   }
+
+  if (error) {
+    return <p className="centered focused">{error}</p>;
+  }
+
+  if (status === 'completed' && !loadedQuote) {
+    return <p>No quote found!</p>;
+  }
+
+  console.log('loaded quote', loadedQuote);
 
   return (
     <>
-      <HighlightedQuote text={quote.text} author={quote.author} />
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
       <Route path={quotePath} exact>
         <div className="centered">
           <Link className="btn--flat" to={commentsURL}>
