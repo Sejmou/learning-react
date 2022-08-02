7fe85cc609b936c2e4101672c9cd84564809c8f7
Sejmou <samo.kolter@gmail.com>
2022-08-02 12:22:42 +0200

    alternative syntax for history.push() param

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -12,9 +12,10 @@ const QuoteList = props => {
   const isSortingAscending = queryParams.get('sort') === 'asc';
 
   const changeSortingHandler = () => {
-    history.push(
-      `${location.pathname}?sort=${isSortingAscending ? 'desc' : 'asc'}`
-    );
+    history.push({
+      pathname: location.pathname,
+      search: `?sort=${isSortingAscending ? 'desc' : 'asc'}`,
+    });
   };
 
   const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
