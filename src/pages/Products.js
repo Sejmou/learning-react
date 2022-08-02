4dfaff8f025d078f74845c598a6754d75b3c002a
Sejmou <samo.kolter@gmail.com>
2022-08-02 20:50:47 +0200

    useHistory (v5) vs. useNavigate (v6)

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,9 +1,23 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+// Change #11: useHistory for imperative routing was replaced by useNavigate
 
 const Products = () => {
+  const navigate = useNavigate();
+
+  const clickHandler = () => {
+    setTimeout(() => {
+      // more realistically, we would use the navigate function in useEffect etc. (e.g. after successful HTTP request)
+      // we have navigation options as well, which we add via a config object
+      navigate('/welcome', { replace: true });
+    }, 2000);
+  };
+
   return (
     <section>
       <h1>The Products Page</h1>
+      <button onClick={clickHandler} className="btn">
+        Pointless button (will redirect after 2 seconds)
+      </button>
       <ul>
         <li>
           <Link to="/products/1">Product 1</Link>
