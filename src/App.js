36be2bed139d9c2934236c91717e442740bb5167
Sejmou <samo.kolter@gmail.com>
2022-08-02 19:52:12 +0200

    example #1 (React Router v5)

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { Route, Switch } from 'react-router-dom';
+
+import MainHeader from './components/MainHeader';
+import Welcome from './pages/Welcome';
+import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
+
+function App() {
+  return (
+    <>
+      <MainHeader />
+      <main>
+        <div>
+          <Switch>
+            <Route path="/welcome">
+              <Welcome />
+            </Route>
+            <Route path="/products" exact>
+              <Products />
+            </Route>
+            <Route path="/products/:productId">
+              <ProductDetail />
+            </Route>
+          </Switch>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
