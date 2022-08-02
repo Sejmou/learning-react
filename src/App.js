d8159182c11251b43d37b181f735ab0f60f8ff89
Sejmou <samo.kolter@gmail.com>
2022-08-02 09:21:02 +0200

    basic setup for new practice project

--- src/App.js
+++ src/App.js
@@ -1,33 +1,8 @@
-import { Redirect, Route, Switch } from 'react-router-dom';
-
-import MainHeader from './components/MainHeader';
-import Welcome from './pages/Welcome';
-import Products from './pages/Products';
-import ProductDetail from './pages/ProductDetail';
-
 function App() {
   return (
-    <>
-      <MainHeader />
-      <main>
-        <div>
-          <Switch>
-            <Route path="/" exact>
-              <Redirect to="/welcome" />
-            </Route>
-            <Route path="/welcome">
-              <Welcome />
-            </Route>
-            <Route path="/products" exact>
-              <Products />
-            </Route>
-            <Route path="/products/:productId">
-              <ProductDetail />
-            </Route>
-          </Switch>
-        </div>
-      </main>
-    </>
+    <div>
+
+    </div>
   );
 }
 
