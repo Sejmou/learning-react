5d2026d3f85e593e70d96aa1ebcf22f8755e9a3d
Sejmou <samo.kolter@gmail.com>
2022-08-02 09:44:42 +0200

    Basic navigation logic (my solution)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { Route, Switch } from 'react-router-dom';
+import MainNavigation from './components/layout/MainNavigation';
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <>
+      <MainNavigation />
+      <main>
+        <div>
+          <Switch>
+            <Route path="/quotes" exact>
+              Quotes Main Page comes here
+            </Route>
+            <Route path="/quotes/:quoteId">Quote Subpage comes here</Route>
+            <Route path="/new-quote">New Quote Page comes here</Route>
+          </Switch>
+        </div>
+      </main>
+    </>
   );
 }
 
