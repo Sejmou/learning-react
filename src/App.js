cbcb7a13dce7e64a9a713649589a69ea6a61bcad
Sejmou <samo.kolter@gmail.com>
2022-08-03 11:35:30 +0200

    reduce initial bundle size w/ lazy loading

--- src/App.js
+++ src/App.js
@@ -1,31 +1,47 @@
+import React, { Suspense } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
-import AllQuotes from './pages/AllQuotes';
-import QuoteDetail from './pages/QuoteDetail';
-import NewQuote from './pages/NewQuote';
-import NotFound from './pages/NotFound';
+// those components are almost guaranteed to be used in the App
 import Layout from './components/layout/Layout';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+import AllQuotes from './pages/AllQuotes';
+
+// those components maybe aren't even needed  for most use-cases (most users might just look at the main page)
+// -> fetch those components from the server lazily (i.e. only when they are actually needed bc the user navigates to the route they're on)
+const QuoteDetail = React.lazy(() => import('./pages/QuoteDetail'));
+const NewQuote = React.lazy(() => import('./pages/NewQuote'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path='/' exact>
-          <Redirect to='/quotes' />
-        </Route>
-        <Route path='/quotes' exact>
-          <AllQuotes />
-        </Route>
-        <Route path='/quotes/:quoteId'>
-          <QuoteDetail />
-        </Route>
-        <Route path='/new-quote'>
-          <NewQuote />
-        </Route>
-        <Route path='*'>
-          <NotFound />
-        </Route>
-      </Switch>
+      {/* Of course, React needs to show something while the lazy-loaded components are being fetched
+          -> provide a JSX element as fallback prop of Suspense, wrap lazy-loaded components with Suspense */}
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
+          <Route path="/quotes" exact>
+            <AllQuotes />
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetail />
+          </Route>
+          <Route path="/new-quote">
+            <NewQuote />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
