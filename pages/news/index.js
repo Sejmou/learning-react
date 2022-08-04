82bb16866dae9f50cf247e32841d29520564e543
Sejmou <samo.kolter@gmail.com>
2022-08-04 10:02:17 +0200

    using Link component for site-internal links

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,4 +1,22 @@
+import Link from 'next/link';
+
 const News = () => {
-  return <div>News</div>;
+  return (
+    <>
+      <h1>News</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-great">
+            NextJS officially declared greatest React Framework
+          </Link>
+        </li>
+        <li>
+          <Link href="/news/angular-sucks">
+            Angular: The most dreaded Frontend Framework
+          </Link>
+        </li>
+      </ul>
+    </>
+  );
 };
 export default News;
