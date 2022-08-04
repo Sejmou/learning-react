4f0a00233adb65024b2c4521f7200dfb40c9e794
Sejmou <samo.kolter@gmail.com>
2022-08-04 10:07:00 +0200

    starting files for bigger Next project

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-// the filename is always used as route pathname for all components in pages
-// index.js is the only exception! just like index.html, it's kinda the default page
-// it is rendered when a request directly to the domain the server runs on is made (not a subpage)
-
-const HomePage = () => {
-  return <div>HomePage</div>;
-};
-export default HomePage;
