1c4ea7af2ddc16561b4e43ff204c1e17b51b2c7d
Sejmou <samo.kolter@gmail.com>
2022-08-04 10:19:03 +0200

    add NewMeetupForm to new-meetup page

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,10 @@
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+
+const NewMeetupPage = () => {
+  function addMetupHandler(enteredMeetupData) {
+    console.log(enteredMeetupData);
+  }
+
+  return <NewMeetupForm onAddMeetup={addMetupHandler} />;
+};
+export default NewMeetupPage;
