7794f2d0b12592783890841db7732f2402361dfe
Sejmou <samo.kolter@gmail.com>
2022-08-04 10:38:59 +0200

    add route to meetup details + component rendering them

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,13 @@
+import MeetupDetails from '../../components/meetups/MeetupDetails';
+
+const MeetupDetailsPage = () => {
+  return (
+    <MeetupDetails
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+      title="First Meetup"
+      description="Some description comes here"
+      address="Some Address 13, 1060 Vienna"
+    />
+  );
+};
+export default MeetupDetailsPage;
