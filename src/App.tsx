2c050a2b7818b6c1748cdd42e14b5b819d8f924f
Sejmou <samo.kolter@gmail.com>
2022-08-05 11:38:49 +0200

    typed useState (making use of generics)

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
+import { useState } from 'react';
+
 import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
 function App() {
-  const todos = [
-    new Todo('Learn React'),
-    new Todo('Learn Next.js'),
-    new Todo('Learn everything'),
-  ];
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const addTodoHandler = (todoText: string) => {
-    console.log('received new TODO:', todoText);
-    // TODO: implement logic for adding Todo
+    const newTodo = new Todo(todoText);
+
+    setTodos(prev => [...prev, newTodo]);
   };
 
   return (
     <div>
-      <Todos todos={todos} />
       <NewTodo onAddTodo={addTodoHandler} />
+      <Todos todos={todos} />
     </div>
   );
 }
