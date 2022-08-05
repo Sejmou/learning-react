f5d6a8dc0c3b5b7d064d307f7c4d423d6c3b520e
Sejmou <samo.kolter@gmail.com>
2022-08-05 11:09:03 +0200

    simple component + prop typing example

---
+++ src/components/Todos.tsx
@@ -0,0 +1,10 @@
+const Todos: React.FC<{ todos: string[] }> = props => {
+  return (
+    <ul>
+      {props.todos.map(todo => (
+        <li key={todo}>{todo}</li>
+      ))}
+    </ul>
+  );
+};
+export default Todos;
