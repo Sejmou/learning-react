01c85bf852191866d0153e63212e0591569404e0
Sejmou <samo.kolter@gmail.com>
2022-08-06 10:55:54 +0200

    basic react-transition-group demo (not yet useful)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Transition from 'react-transition-group/Transition';
 
 import './App.css';
 import Modal from './components/Modal/Modal';
@@ -8,6 +9,7 @@ import List from './components/List/List';
 class App extends Component {
   state = {
     modalIsOpen: false,
+    showDemoBlock: false,
   };
 
   showModal = () => {
@@ -18,10 +20,47 @@ class App extends Component {
     this.setState({ modalIsOpen: false });
   };
 
+  toggleBlock = () => {
+    this.setState(state => ({ showDemoBlock: !state.showDemoBlock }));
+  };
+
   render() {
     return (
       <div className="App">
         <h1>React Animations</h1>
+        <div
+          style={{
+            margin: '10px auto',
+            maxWidth: '400px',
+          }}
+        >
+          <h3>Simple react-transition-group demo</h3>
+          <Transition in={this.state.showDemoBlock}>
+            {state => <p>Current transition state: {state}</p>}
+          </Transition>
+          <p>Click the button below and observe the change!</p>
+          <div
+            style={{
+              display: 'flex',
+              margin: '0 auto',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              height: 100,
+            }}
+          >
+            {this.state.showDemoBlock && (
+              <div
+                style={{ backgroundColor: 'red', width: 100, height: 100 }}
+              ></div>
+            )}
+            <div style={{ flex: 1 }}></div>
+            <div>
+              <button className="Button" onClick={this.toggleBlock}>
+                Toggle Block
+              </button>
+            </div>
+          </div>
+        </div>
         {this.state.modalIsOpen && <Modal onClose={this.closeModal} />}
         {this.state.modalIsOpen && <Backdrop onClick={this.closeModal} />}
         <button className="Button" onClick={this.showModal}>
