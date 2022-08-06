d3741b434eeb6ebfe93b8690191377bfa6d5c9f8
Sejmou <samo.kolter@gmail.com>
2022-08-06 11:33:09 +0200

    animate Modal with Transition

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import Modal from './components/Modal/Modal';
 import Backdrop from './components/Backdrop/Backdrop';
 import List from './components/List/List';
 
-const transitionDuration = 1000;
-
 class App extends Component {
   state = {
     modalIsOpen: false,
@@ -30,60 +28,19 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <div
-          style={{
-            margin: '10px auto',
-            maxWidth: '400px',
-          }}
+        <Transition
+          in={this.state.modalIsOpen}
+          timeout={400}
+          mountOnEnter
+          unmountOnExit
         >
-          <h3>Simple react-transition-group demo</h3>
-          <p>Click 'Toggle Block' button and observe the changes!</p>
-          <p>showDemoBlock: {`${this.state.showDemoBlock}`}</p>
-          <Transition
-            in={this.state.showDemoBlock}
-            timeout={transitionDuration}
-            mountOnEnter
-            unmountOnExit
-          >
-            {state => <p>transition state: {state}</p>}
-          </Transition>
-          <div
-            style={{
-              display: 'flex',
-              margin: '0 auto',
-              alignItems: 'center',
-              justifyContent: 'space-between',
-              height: 100,
-            }}
-          >
-            <Transition
-              in={this.state.showDemoBlock}
-              timeout={transitionDuration}
-              mountOnEnter
-              unmountOnExit
-            >
-              {state => (
-                <div
-                  style={{
-                    backgroundColor: 'red',
-                    width: 100,
-                    height: 100,
-                    transition: `opacity ${transitionDuration / 1000}s`,
-                    opacity: state === 'entered' ? 1 : 0,
-                  }}
-                ></div>
-              )}
-            </Transition>
-            <div style={{ flex: 1 }}></div>
-            <div>
-              <button className="Button" onClick={this.toggleBlock}>
-                Toggle Block
-              </button>
-            </div>
-          </div>
-        </div>
-        {this.state.modalIsOpen && <Modal onClose={this.closeModal} />}
-        {this.state.modalIsOpen && <Backdrop onClick={this.closeModal} />}
+          {state => (
+            <>
+              <Modal transitionState={state} onClose={this.closeModal} />
+              <Backdrop onClick={this.closeModal} />
+            </>
+          )}
+        </Transition>
         <button className="Button" onClick={this.showModal}>
           Open Modal
         </button>
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,13 +2,23 @@ import React from 'react';
 
 import './Modal.css';
 
-const Modal = props => (
-  <div className="Modal">
-    <h1>A Modal</h1>
-    <button className="Button" onClick={props.onClose}>
-      Dismiss
-    </button>
-  </div>
-);
+const Modal = props => {
+  const cssClasses = getCssClasses(props.transitionState);
 
+  return (
+    <div className={cssClasses}>
+      <h1>A Modal</h1>
+      <button className="Button" onClick={props.onClose}>
+        Dismiss
+      </button>
+    </div>
+  );
+};
 export default Modal;
+
+function getCssClasses(transitionState) {
+  const classes = ['modal'];
+  if (transitionState === 'entering') classes.push('modal--opening');
+  if (transitionState === 'exiting') classes.push('modal--closing');
+  return classes.join(' ');
+}
